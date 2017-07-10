6c5675a1bf83c1c7ccd35ff32938fd34d1884338
Raphealkxy <1277609958@qq.com>
2017-07-10 15:27:31 +0800

    add fristpagerAdapter

--- app/src/main/java/com/example/timmy/splashactivity/Activity/Activity/Pager/firstpager.java
+++ app/src/main/java/com/example/timmy/splashactivity/Activity/Activity/Pager/firstpager.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.graphics.Color;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.timmy.splashactivity.Activity.Activity.adapter.firstPagerAdapter;
 import com.example.timmy.splashactivity.Activity.Activity.base.BasePager;
 import com.example.timmy.splashactivity.Activity.Activity.utils.LogUtil;
+import com.example.timmy.splashactivity.R;
 
 /**
  * Created by Timmy on 2017/7/9.
@@ -16,7 +21,9 @@ import com.example.timmy.splashactivity.Activity.Activity.utils.LogUtil;
 public class firstpager extends BasePager {
 
 
-    private TextView textView;
+    private ListView listView;
+    private String[] datas;
+    private firstPagerAdapter mfirstPagerAdapter;
     public firstpager(Context content) {
         super(content);
     }
@@ -24,21 +31,26 @@ public class firstpager extends BasePager {
     @Override
     public View initVeiw() {
       LogUtil.e("首页已经被初始化了");
-        textView=new TextView(context);
-        textView.setTextSize(25);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-
-
-        return textView;
+      View view=View.inflate(context, R.layout.first_fragment,null);
+      listView= (ListView) view.findViewById(R.id.listview);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String data=datas[position];
+                Toast.makeText(context,data,Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 
     @Override
     public void initdata() {
         super.initdata();
         LogUtil.e("首页的数据被初始化了");
-
-        textView.setText("你好，这是首页");
-
+        //准备数据
+        datas=new String[]{"OKHttp", "xUtils3","Retrofit2","Fresco","Glide","greenDao","RxJava","volley","Gson","FastJson","picasso","evenBus","jcvideoplayer","pulltorefresh","Expandablelistview","UniversalVideoView","....."};
+    //设置适配器
+        mfirstPagerAdapter=new firstPagerAdapter(context,datas);
+         listView.setAdapter(mfirstPagerAdapter);
     }
 }
